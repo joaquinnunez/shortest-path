8c10b4f192d4639044da07f0a019a64ab3c84cd6
foo <foo@Foos-MacBook-Pro.local>
2020-03-05 16:07:30 -0300

    Looking for the shortest path, 1st try

--- rutadetren.py
+++ rutadetren.py
@@ -1,11 +1,35 @@
 import argparse
 import csv
+from collections import defaultdict
 
 parser = argparse.ArgumentParser(description='Calcula la menor distancia entre 2 estaciones de tren')
 parser.add_argument('file', help='Archivo de texto a leer')
 args = parser.parse_args()
 
+inf = float('inf')
+dist = {}
+prev = {}
+neighbours = defaultdict(list)
+
 with open(args.file, 'r') as csvfile:
   reader = csv.reader(csvfile)
-  for row in reader:
-      print ', '.join(row)
+  for station1, station2, distance in reader:
+    dist[station1] = inf
+    dist[station2] = inf
+    prev[station1] = None
+    prev[station2] = None
+    neighbours[station1].append((station2, int(distance)))
+    neighbours[station2].append((station1, int(distance))) # bidirectional
+
+unvisited = set(dist.keys())
+dist[station1] = 0
+
+while unvisited: # not empty
+  node = min(unvisited, key=lambda uv: dist[uv]) # use a better data-structure
+  unvisited.remove(node)
+  for end, cost in neighbours[node]:
+    if dist[node] + cost < dist[end]:
+      dist[end] = dist[node] + cost
+      prev[end] = node
+
+print(dist)
